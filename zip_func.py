5e82af00da46c97aea194eede08e8a39fce2bdc9
takfarivus <80676974+takfarivas@users.noreply.github.com>
2021-03-16 15:43:50 +0100

    Update zip_func.py

--- zip_func.py
+++ zip_func.py
@@ -1,9 +1,13 @@
+# Items
 items = ['Apples', 'Bananas', 'Oranges']
-counts = [4, 6, 2]
-prices = [0.59, 0.45, 0.99]
+# Counts for how much items we buy
+counts = [6, 7, 2]
+# The prices for each item
+prices = [0.55, 0.99, 0.45]
 
+# Zipping the items, counts and prices
 zipped = list(zip(items, counts, prices))
 
+# Printing how much we have to get for buying each
 for (item, count, price) in zipped:
     print(f'If you buy {count} {item} you will pay {count * price} $')
-
